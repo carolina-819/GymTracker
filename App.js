230e8fa6e1b36a27b06f0a097cc27414c7f90029
Carolina <up201705692@fe.up.pt>
2022-01-17 03:51:22 +0000

    Connection to firebase done
    shows ugly arm exercises, form not working

--- App.js
+++ App.js
@@ -1,36 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {Appbar, Button, DarkTheme, DefaultTheme, Provider as PaperProvider, useTheme} from 'react-native-paper';
-import {createMaterialTopTabNavigator} from "@react-navigation/material-top-tabs";
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Appbar, FAB } from 'react-native-paper';
+import { FormBuilder } from "react-native-paper-form-builder";
+import { useForm } from "react-hook-form";
+import { deleteDoc, collection, doc, addDoc, getFirestore, onSnapshot, query, where, orderBy, arrayRemove, updateDoc, clearIndexedDbPersistence, getDoc, getDocs } from "firebase/firestore";
+import './firebase'
 
-const Tab = createMaterialTopTabNavigator();
+function Bracos({navigation}) {
+  const [arms, setArms] = useState([]);
+  useEffect(() => {
+    const db = getFirestore();
+    const _query = query(collection(db, "exercicios"), where('Categoria', '==', 'Braços'));
+    const unsubscribe = onSnapshot(_query, (data) => {
+      setArms(data.docs.map(doc => {
 
-function Braços() {
-  return (
-    <View><Text>Aqui veem se os exercicios de braços</Text></View>
-  )
+        const data = doc.data();
+        data.id = doc.id;
+        console.log(doc.Nome);
+        return data;
+      }))
+    })
+
+    return unsubscribe;
+  }, []);
+  return ( //cada exercicio mostra o nome, tu carregas e da drop down, quando da dropdown mostrar o botao de editar e apagar
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Os teus exercicios</Text>
+      {arms.map((braco) => (
+        <View>
+        <Text key={braco.id} >{"Nome " + braco.Nome}</Text>
+        <Text>{"Sets: " + braco.Sets}</Text>
+        <Text>{"Reps: " + braco.Reps}</Text>
+        <Text>{"último peso: " + braco.Peso + braco.Medida}</Text>
+        <Text>   </Text>
+        </View>
+      ))}
+      <FAB
+            small
+            icon="plus"
+            label="Adicionar novo exercicio"
+            uppercase
+            onPress={() => navigation.navigate('NewExercise', {type: "arms"})} //mudar para ir para o ecrã de novo carregador
+          />
+      </View>
+  );
 }
-function Pernas() {
-  return (
-    <View><Text>Aqui veem se os exercicios de pernas</Text></View>
-  )
-}
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Tab.Navigator>
-      <Tab.Screen name="Braços" component={Braços} />
-      <Tab.Screen name="Pernas" component={Pernas} />
-    </Tab.Navigator>
-    </View>
+function NewExercise(props){
+  const { control, setFocus, handleSubmit } = useForm({
+    mode: 'onChange',
+  });
+  return(
+    
+    <View style={{ margin: 20 }}>
+     <Text>{"woeoea adicionar: " + props.route.params.type}</Text>
+        </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Pernas({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Aqui veem se os exercicios de pernas</Text></View>
+  );
+}
+const Tab = createMaterialTopTabNavigator();
+
+function HomeScreen({ navigation }){
+  return(
+    <Tab.Navigator>
+          <Tab.Screen name="Upper body day bby" component={Bracos} />
+          <Tab.Screen name="Never skip leg day" component={Pernas} />
+        </Tab.Navigator>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NewExercise" component={NewExercise} />
+      </Stack.Navigator>
+        
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
+
+
+/**/
---
+++ firebase/index.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTfVYaNvpqv3Lt2LrXZlqbk1qPTn5Nc1M",
+  authDomain: "gym-tracker-d89af.firebaseapp.com",
+  projectId: "gym-tracker-d89af",
+  storageBucket: "gym-tracker-d89af.appspot.com",
+  messagingSenderId: "817651943275",
+  appId: "1:817651943275:web:ad27d98f4502b590fa6e2e",
+  measurementId: "G-4QYHLWDCNF"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
