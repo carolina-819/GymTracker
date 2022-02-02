36e75a4545b6564abaceb76d6fe3889f4e1265b3
Carolina <up201705692@fe.up.pt>
2022-02-02 17:49:33 +0000

    Added single exercise page
    with static buttons to delete and edit

--- App.js
+++ App.js
@@ -1,66 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import { Appbar, FAB, List, Card, Divider, IconButton, Dialog, Portal, Button, Provider as PaperProvider } from 'react-native-paper';
+import { Appbar, FAB, CardTitle, List, Card, Divider, IconButton, Dialog, Portal, Button, Provider as PaperProvider } from 'react-native-paper';
 import { FormBuilder } from "react-native-paper-form-builder";
 import { useForm } from "react-hook-form";
-import { deleteDoc, collection, doc, addDoc, getFirestore, onSnapshot, query, where, orderBy, arrayRemove, updateDoc, clearIndexedDbPersistence, getDoc, getDocs } from "firebase/firestore";
+import { deleteDoc, collection, doc, addDoc, getFirestore, onSnapshot, query, where, orderBy, arrayRemove, updateDoc, clearIndexedDbPersistence, getDoc, getDocs, limitToLast } from "firebase/firestore";
 import './firebase'
 
-function Bracos({ navigation }) {
-  const [arms, setArms] = useState([]);
-  const [dialog, setDialog] = useState(false);
-  const hide = () => setDialog(false);
-  const show = () => setDialog(true);
 
-  useEffect(() => {
-    const db = getFirestore();
-    const _query = query(collection(db, "exercicios"), where('Categoria', '==', 'Braços'));
-    const unsubscribe = onSnapshot(_query, (data) => {
-      setArms(data.docs.map(doc => {
-
-        const data = doc.data();
-        data.id = doc.id;
-        console.log(doc.Nome);
-        return data;
-      }))
-    })
-
-    return unsubscribe;
-  }, []);
-
-  return ( //cada exercicio mostra o nome, tu carregas e da drop down, quando da dropdown mostrar o botao de editar e apagar
-    <View style={{ flex: 1 }}>
-      <Card.Title title="Os teus exercicios" />
-      <List.AccordionGroup>
-        {arms.map((braco) => (
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <View style={{flex: 1}}>
-            <List.Accordion title={braco.Nome} id={braco.id} onLongPress={show} style={{backgroundColor: 'transparent'}}>
-              <List.Item title={"Sets: " + braco.Sets} />
-              <List.Item title={"Reps: " + braco.Reps} />
-              <List.Item title={"último peso: " + braco.Peso + braco.Medida} />
-            </List.Accordion>
-            </View>
-            <IconButton onPress={() => console.log('Pressed')} icon="delete"/>
-            <IconButton onPress={() => console.log('Pressed')} icon="pencil" />
-          </View>
-        ))}
-      </List.AccordionGroup>
-
-      <FAB
-        small
-        icon="plus"
-        label="Adicionar novo exercicio"
-        uppercase
-        onPress={() => navigation.navigate('New Exercise', { type: "arms" })} //mudar para ir para o ecrã de novo carregador
-      />
-    </View>
-  );
-}
 function NewExercise(props) {
   const { navigation, route } = props;
   const { control, setFocus, handleSubmit } = useForm({
@@ -80,7 +30,7 @@ function NewExercise(props) {
         Peso: Number(data.peso),
         Reps: Number(data.reps),
         Sets: Number(data.sets),
-        Categoria: (props.route.params.type == 'arms') ? "Braços" : "Pernas" ,
+        Categoria: (props.route.params.type == 'arms') ? "Braços" : "Pernas",
         Medida: data.medida
       });
 
@@ -105,7 +55,8 @@ function NewExercise(props) {
         control={control}
         setFocus={setFocus}
         formConfigArray={[
-          {name: 'nome',
+          {
+            name: 'nome',
             type: 'text',
             textInputProps: {
               label: 'Name',
@@ -117,13 +68,15 @@ function NewExercise(props) {
               },
             },
           },
-          {name: 'notas',
+          {
+            name: 'notas',
             type: 'text',
             textInputProps: {
               label: 'Notas',
             },
           },
-          {name: 'peso',
+          {
+            name: 'peso',
             type: 'text',
             textInputProps: {
               label: 'Peso',
@@ -135,7 +88,8 @@ function NewExercise(props) {
               },
             },
           },
-          {name: 'medida',
+          {
+            name: 'medida',
             type: 'select',
             textInputProps: {
               label: 'Medida',
@@ -159,7 +113,8 @@ function NewExercise(props) {
               },
             ],
           },
-          {name: 'sets',
+          {
+            name: 'sets',
             type: 'text',
             textInputProps: {
               label: 'Sets',
@@ -170,9 +125,10 @@ function NewExercise(props) {
                 message: 'Sets é obrigatório',
               },
             },
-            
+
           },
-          { name: 'reps',
+          {
+            name: 'reps',
             type: 'text',
             textInputProps: {
               label: 'Reps',
@@ -183,20 +139,77 @@ function NewExercise(props) {
                 message: 'Reps é obrigatório',
               },
             },
-            
+
           },
 
         ]} />
-        <Button mode="contained" disabled={pending} onPress={handleSubmit(submit)}>
-          Guardar Alterações
-        </Button>
-        <Button mode="contained" onPress={navigation.goBack} color="#787878">
-          Cancelar
-        </Button>
+      <Button mode="contained" disabled={pending} onPress={handleSubmit(submit)}>
+        Guardar Alterações
+      </Button>
+      <Button mode="contained" onPress={navigation.goBack} color="#787878">
+        Cancelar
+      </Button>
     </View>
   );
 }
 
+function EditExercise(props) {
+}
+function Bracos({ navigation }) {
+  const [arms, setArms] = useState([]);
+  const [dialog, setDialog] = useState(false);
+  const hide = () => setDialog(false);
+  const show = () => setDialog(true);
+
+  useEffect(() => {
+    const db = getFirestore();
+    const _query = query(collection(db, "exercicios"), where('Categoria', '==', 'Braços'));
+    const unsubscribe = onSnapshot(_query, (data) => {
+      setArms(data.docs.map(doc => {
+
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
+      }))
+    })
+
+    return unsubscribe;
+  }, []);
+
+  return ( //cada exercicio mostra o nome, tu carregas e da drop down, quando da dropdown mostrar o botao de editar e apagar
+    <ScrollView>
+      <Card.Title title="Os teus exercicios" />
+      {arms.map((braco) => (
+        <View>
+
+          <List.Item left={props =>
+            <List.Icon {...props}
+              icon="eye"
+            />
+          }
+            onPress={() => navigation.navigate('Single Exercise', { id: braco.id })}
+            title={braco.Nome}
+            key={braco.id}
+            style={{ backgroundColor: 'grey' }} />
+          <List.Item title={"Sets: " + braco.Sets} />
+          <List.Item title={"Reps: " + braco.Reps} />
+          <List.Item title={"último peso: " + braco.Peso + braco.Medida} />
+
+
+
+        </View>
+      ))}
+
+      <FAB
+        small
+        icon="plus"
+        label="Adicionar novo exercicio"
+        uppercase
+        onPress={() => navigation.navigate('New Exercise', { type: "arms" })} //mudar para ir para o ecrã de novo carregador
+      />
+    </ScrollView>
+  );
+}
 function Pernas({ navigation }) {
   const [legs, setLegs] = useState([]);
   const [dialog, setDialog] = useState(false);
@@ -222,21 +235,16 @@ function Pernas({ navigation }) {
   return ( //cada exercicio mostra o nome, tu carregas e da drop down, quando da dropdown mostrar o botao de editar e apagar
     <View style={{ flex: 1 }}>
       <Card.Title title="Os teus exercicios" />
-      <List.AccordionGroup>
-        {legs.map((perna) => (
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <View style={{flex: 1}}>
-            <List.Accordion title={perna.Nome} id={perna.id} onLongPress={show} style={{backgroundColor: 'transparent'}}>
-              <List.Item title={"Sets: " + perna.Sets} />
-              <List.Item title={"Reps: " + perna.Reps} />
-              <List.Item title={"último peso: " + perna.Peso + perna.Medida} />
-            </List.Accordion>
-            </View>
-            <IconButton onPress={() => console.log('Pressed')} icon="delete"/>
-            <IconButton onPress={() => console.log('Pressed')} icon="pencil" />
-          </View>
-        ))}
-      </List.AccordionGroup>
+      {legs.map((perna) => (
+        <View>
+          <List.Item title={perna.Nome} key={perna.id} style={{ backgroundColor: 'transparent' }} />
+          <List.Item title={"Sets: " + perna.Sets} />
+          <List.Item title={"Reps: " + perna.Reps} />
+          <List.Item title={"último peso: " + perna.Peso + perna.Medida} />
+
+        </View>
+
+      ))}
 
       <FAB
         small
@@ -245,6 +253,58 @@ function Pernas({ navigation }) {
         uppercase
         onPress={() => navigation.navigate('New Exercise', { type: "legs" })} //mudar para ir para o ecrã de novo carregador
       />
+    </View>
+  );
+}
+function SingleEx(props) {
+  const { navigation, route } = props;
+  const [ex, setEx] = useState({});
+
+  useEffect(async () => {
+    const db = getFirestore();
+    const _query = doc(db, "exercicios", props.route.params.id);
+    const docSnap = await getDoc(_query);
+    if (docSnap.exists()) {
+      setEx(docSnap.data());
+    }
+
+    return docSnap;
+  }, []);
+  return (
+    <View>
+      <Card.Title title={ex.Nome} right={() => (
+        <View style={{flexDirection: 'row'}}>
+        <IconButton
+          icon="pencil"
+          size={20}
+          onPress={() => console.log('Editar')}
+        />
+        <IconButton
+          icon="delete"
+          size={20}
+          onPress={() => console.log('Apagar')}
+        />
+        </View>
+      )} />
+
+      <List.Item title={"Sets"} right={() => (
+        <View>
+          <Text>{ex.Sets}</Text>
+        </View>
+      )} />
+      <List.Item title={"Reps"} right={() => (
+        <View>
+          <Text>{ex.Reps}</Text>
+        </View>
+      )} />
+      <List.Item title={"Peso"} right={() => (
+        <View>
+          <Text>{ex.Peso + " " + ex.Medida}</Text>
+        </View>
+      )} />
+      <List.Item title={"Notas"} description={ex.Notas} />
+
+
     </View>
   );
 }
@@ -262,20 +322,21 @@ function HomeScreen({ navigation }) {
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-   
+
     <SafeAreaProvider>
-       <PaperProvider>
+      <PaperProvider>
 
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="New Exercise" component={NewExercise} />
-        </Stack.Navigator>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="New Exercise" component={NewExercise} />
+            <Stack.Screen name="Single Exercise" component={SingleEx} />
+          </Stack.Navigator>
 
-      </NavigationContainer>
+        </NavigationContainer>
       </PaperProvider>
     </SafeAreaProvider>
-    
+
   );
 }
 
